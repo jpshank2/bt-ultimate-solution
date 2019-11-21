51d9044f31b5765c8c18e63a61cf894864b71ca4
Jeremy Shank <jpshank2@gmail.com>
2019-11-21 11:02:07 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import './App.css';
+import Employer from './Pages/Employer';
+import Student from './Pages/Student';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route exact path="/" render={() => (
+        <React.Fragment>
+          <h1>Are you a:</h1>
+          <Link to="/employer">Employer</Link>
+          <Link to="/student">Student</Link>
+        </React.Fragment>
+      )} />
+      <Route exact path="/employer" render={() => (
+        <Employer />
+      )} />
+      <Route exact path="/student" render={() => (
+        <Student />
+      )} />
+    </Router>
   );
 }
 
---
+++ src/Pages/Student.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+
+export default class Student extends Component {
+    render() {
+        return (
+            <div>
+                You are a student!
+            </div>
+        )
+    }
+}
